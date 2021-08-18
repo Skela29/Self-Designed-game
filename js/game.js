0e7c865b886134124ab8edce47d25471a81304ec
Skela29 <84703226+Skela29@users.noreply.github.com>
2021-08-18 14:58:13 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -2,12 +2,73 @@ class Game{
     constructor(){
 
     }
-    
+    start(){
+        mammals = createSprite(displayWidth/2-250,displayHeight/2,50,50)
+        mammals.addImage(mammalsImg)
+        mammals.scale=0.1
+        amphibians = createSprite(displayWidth/2,displayHeight/2,50,50)
+        amphibians.addImage(amphibiansImg)
+        amphibians.scale=0.1
+        reptiles = createSprite(displayWidth/2+250,displayHeight/2,50,50)
+        reptiles.addImage(reptilesImg)
+        reptiles.scale=0.1
+        frog = createSprite(width-100,height-100)
+        frog.addImage(frogImg)
+        frog.scale=0.3
+        cow = createSprite(200,height/2+50)
+        cow.addImage(cowImg)
+        cow.scale=0.4
+        snake = createSprite(width-250,height/4-40)
+        snake.addImage(snakeImg)
+        snake.scale=0.5  
+    }
     play(){
         background(animalBg)
-        mammals = createSprite(displayWidth/2-250,displayHeight/2,50,50)
-        amphibians = createSprite(displayWidth/2,displayHeight/2,50,50)
-        reptiles = createSprite(displayWidth/2+250,displayHeight/2,50,50)
+        textSize(30)
+        text("Score:"+score,50,50)
+
+// Cow
+        if(cow.isTouching(mammals)){
+            cow.destroy();
+            score = score+10
+        }
+        if(cow.isTouching(reptiles)||cow.isTouching(amphibians)){
+            cow.x = 200
+            cow.y = height/2+50
+            score-=5
+        }
+        if(mousePressedOver(cow)){
+            cow.x=mouseX
+            cow.y=mouseY
+        }
+// Snake
+        if(snake.isTouching(reptiles)){
+            snake.destroy();
+            score = score+10
+        }
+        if(snake.isTouching(reptiles)||snake.isTouching(reptiles)){
+            snake.x = width-250
+            snake.y = height/4-40
+            score-=5
+        }
+        if(mousePressedOver(snake)){
+            snake.x=mouseX
+            snake.y=mouseY
+        }
+// Frog
+        if(frog.isTouching(amphibians)){
+            frog.destroy();
+            score = score+10
+        }
+        if(frog.isTouching(amphibians)||frog.isTouching(amphibians)){
+            frog.x = width-100
+            frog.y = height-100
+            score-=5
+        }
+        if(mousePressedOver(frog)){
+            frog.x=mouseX
+            frog.y=mouseY
+        }
         drawSprites();
         }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,22 @@
 var form, gameState=0,play
 var mammals,amphibians,reptiles
+var cow,snake,frog
+var score=0
 function preload(){
   animalBg=loadImage("Images/AnimalBg.jpg")
+  cowImg = loadImage("Images/Cow.png")
+  frogImg = loadImage("Images/frog.png")
+  snakeImg = loadImage("Images/Snake.png")
+
+  reptilesImg = loadImage("Images/Reptile.png")
+  amphibiansImg = loadImage("Images/Amphibian.png")
+  mammalsImg = loadImage("Images/Mammals.jpg")
 }
 function setup() {
-  createCanvas(displayWidth,displayHeight);
+  createCanvas(displayWidth,displayHeight-50);
 form = new Form();
 game = new Game();
+game.start();
 }
 
 function draw() {
